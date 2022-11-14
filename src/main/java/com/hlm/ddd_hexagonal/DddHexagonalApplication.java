453fc9ef713c5fd39c8104cf969df4775ca2911e
Mauricio Ccasani Olivares <mccasani@emeal.nttdata.com>
2022-11-13 22:50:05 -0500

    fix import

--- src/main/java/com/hlm/ddd_hexagonal/DddHexagonalApplication.java
+++ src/main/java/com/hlm/ddd_hexagonal/DddHexagonalApplication.java
@@ -1,16 +1,14 @@
 package com.hlm.ddd_hexagonal;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DddHexagonalApplication implements CommandLineRunner {
+public class DddHexagonalApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(DddHexagonalApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {}
+
 }
